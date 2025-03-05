4d4945cd536ac0476ffac57c843d3cb160edd3cb
sivasaileo <learningpractice999@gmail.com>
2025-03-05 18:50:01 +0530

    S3

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 # Define an S3 bucket resource
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-sai-s3-bucket-name"  # Replace with a unique bucket name
+  bucket = "my-sainath-s3-bucket-name"  # Replace with a unique bucket name
 
   # Enable versioning for the bucket (optional)
   versioning {
